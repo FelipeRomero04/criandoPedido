38f72d3f17bdb4c3e45d2db4f679cea6961a4903
Felipe Romero <felipermrcosta@gmail.com>
2025-10-27 00:16:48 -0300

    implementando modularização

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,23 @@
-import org.example.Dao.ProductDao;
-import org.example.entitys.Order;
-import org.example.entitys.Product;
-import org.example.service.ServiceOrder;
 import org.example.service.ServiceOrders;
-
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.util.List;
-
 import static org.example.utils.connectionFactory.Connectivity.connectionDb;
 
 public class Main{
     public static void main(String[] args) throws SQLException {
 
-        Connection conn = null;
+        Connection conn = connectionDb();
         try{
-            conn = connectionDb();
-            ServiceOrders order = new ServiceOrders(conn);
-            conn.setAutoCommit(false);
-
-            Order order1 = new Order(2, "2025-05-10 17:40:20", 0.0);
-
-            int order_id = order.saveOrder(order1);
-            order.fazendoPedido(order_id, List.of(4, 5, 6), List.of(2, 3, 7));
-
-            conn.commit();
-        }catch (NullPointerException e){
-            if(conn != null) conn.rollback();
-            e.printStackTrace();
+            ServiceOrders servOrder = new ServiceOrders(conn);
+            servOrder.orderTransaction();
+        }catch(NullPointerException e){
+            System.out.println(e.getMessage());
         }
-        if(conn != null) conn.close();
     }
 }
 
-//passar isso pro service e o service pro repossitory
+// Terminar de colocar os DAOs e util, etc
+// Anotar reforçar a responsabilidade de cada camada (apenas uma camada controla conexão)
+// Fazer commit sempre que achar que vai estragar tudo
+// Ficar atento com acoplamento
+// Injetar pelo construtor, permite eu usar a classe livremente. (como?)
--- src/main/java/org/example/service/ServiceOrders.java
+++ src/main/java/org/example/service/ServiceOrders.java
@@ -1,83 +1,37 @@
 package org.example.service;
 
 import org.example.entitys.Order;
-import org.example.entitys.Product;
+import org.example.repository.RepoOrder;
 
-import javax.swing.plaf.nimbus.State;
 import java.sql.*;
 import java.util.List;
 
-import static org.example.utils.connectionFactory.Connectivity.connectionDb;
-import static org.example.utils.dateUtils.Utils.productById;
 
 public class ServiceOrders {
     private final Connection conn;
+    private final RepoOrder repoOrder;
 
     public ServiceOrders(Connection conn) {
         this.conn = conn;
+        this.repoOrder = new RepoOrder(conn);
     }
 
-    public int saveOrder(Order order) throws SQLException {
-        String query = "INSERT INTO orders(client_id, date, total_value) VALUES (?, ?, ?);";
-        int order_id;
-        try(PreparedStatement stmt = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)){
+    public void orderTransaction() throws SQLException {
+        try {
+            conn.setAutoCommit(false);
 
-            stmt.setInt(1, order.getClient_id());
-            stmt.setTimestamp(2, Timestamp.valueOf(order.getDate()));
-            stmt.setDouble(3, order.getTotal_value());
+            Order order = new Order(2, "2025-05-10 17:40:20", 0.0);
 
-            stmt.executeUpdate();
-            ResultSet rs = stmt.getGeneratedKeys();
-            rs.next();
-            order_id = rs.getInt(1);
+            int order_id = repoOrder.saveOrder(order);
+            repoOrder.placingOrder(order_id, List.of(4, 5, 6), List.of(2, 3, 7));
+            // passar listas como parâmetros
 
-
-        }catch (SQLException e){
-            throw e;
+            conn.commit();
+        } catch (NullPointerException e) {
+            if (conn != null) conn.rollback();
+            e.printStackTrace();
         }
-        return order_id;
+        if (conn != null) conn.close();
     }
 
-
-    public void fazendoPedido(int order_id,List<Integer> id_products, List<Integer> quantitys) throws SQLException {
-        String insertQuery = "INSERT INTO order_item(order_id, product_id, quantity) VALUES (?,?,?);";
-
-        try (PreparedStatement stmtInsrt = conn.prepareStatement(insertQuery)) {
-            for (int i = 0; i < id_products.size(); i++) {
-                stmtInsrt.setInt(1, order_id);
-                stmtInsrt.setInt(2, id_products.get(i));
-                stmtInsrt.setDouble(3, quantitys.get(i));
-
-                stmtInsrt.addBatch(); //guarda em lotes
-            }
-            stmtInsrt.executeBatch(); //executa tudo
-
-            calc_total(order_id, id_products, quantitys);
-
-            System.out.println("FEITO");
-        } catch (SQLException e) {
-            throw e;
-        }
-    }
-
-    public void calc_total(int order_id ,List<Integer> id_items, List<Integer> quant) throws SQLException {
-        String uptQuery = "UPDATE orders SET total_value = ? WHERE id = ?;";
-        double total = 0;
-
-        try(PreparedStatement stmtUpd = conn.prepareStatement(uptQuery)){
-
-            for (int i = 0; i < id_items.size(); i++) {
-                Product product = productById(id_items.get(i));
-                total += product.getPrice() * quant.get(i);
-            }
-
-            stmtUpd.setDouble(1, total);
-            stmtUpd.setInt(2, order_id);
-            stmtUpd.executeUpdate();
-
-
-        }catch(SQLException e){
-            throw e;
-        }
-    }
 }
